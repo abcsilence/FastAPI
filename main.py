85a94f75ad467881b0ae78d1568b87c1d78dda7b
abcsilence <jaishiroshan7@gmail.com>
2026-05-02 14:20:30 +0545

    learn about the query param

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -28,4 +28,23 @@ def view_patient(patient_id: str = Path(..., description = 'ID of the patient', 
     if patient_id in data:
         return data[patient_id]
 
-    return {'error': 'Patient not found'}
+    raise HTTPException(status_code = 404, detail = 'Patient not found')
+
+
+@app.get('/sort')
+
+def sort_patients(
+    sort_by: str = Query(..., description="Sort on the basis of height, weight or bmi"),
+    order: str = Query('asc', description='sort in asc or desc order')
+):
+    valid_fields = ['height', 'weight', 'bmi','age']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code = 400, detail = "Invalid order select between asc and desc")
+
+    sort_order = True if order == 'desc' else False
+    data = load_data()
+    sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by,0), reverse = sort_order)
+
+
+    return sorted_data
